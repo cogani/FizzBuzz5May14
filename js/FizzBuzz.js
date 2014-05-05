0bd88b4a64641d7e71fb4fd3c5e173dcb126661d
Nico Cortes <info@ncortes.com>
2014-05-05 20:16:48 +0200

    will return 1 when 1 is supplied

---
+++ js/FizzBuzz.js
@@ -0,0 +1,4 @@
+function fizzbuzz(aNumber){
+
+    return 1;
+}
---
+++ spec/FizzBuzzSpec.js
@@ -0,0 +1,9 @@
+describe("FizzBuzz Kata", function(){
+    it("will return 1 when 1 is supplied", function(){
+        var result;
+        result = fizzbuzz(1);
+
+
+        expect(result).toBe(1);
+    })
+})
