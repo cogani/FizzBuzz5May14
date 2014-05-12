8fd358c9e70553af74720973e803cfc23b7d7d5b
Nico Cortes <info@ncortes.com>
2014-05-12 12:16:02 +0200

    Final refactoring - Spec

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,55 +1,28 @@
 describe("FizzBuzz Kata", function(){
-    it("will return 1 when 1 is supplied", function(){
+    it("will return the own number when a 'normal' number is supplied", function(){
         var result;
         result = fizzbuzz(1);
-
 
         expect(result).toBe(1);
     })
 
-    it("will return 2 when 2 is supplied", function(){
-        var result;
-        result = fizzbuzz(2);
-
-
-        expect(result).toBe(2);
-    })
-    it("will return fizz when 3 is supplied", function(){
+    it("will return fizz when a multiple of 3 is supplied", function(){
         var result;
         result = fizzbuzz(3);
 
-
         expect(result).toBe('fizz');
     })
 
-    it("will return buzz when 5 is supplied", function(){
+    it("will return buzz when a multiple of 5 is supplied", function(){
         var result;
         result = fizzbuzz(5);
 
-
         expect(result).toBe('buzz');
     })
 
-    it("will return fizz when 6 is supplied", function(){
-        var result;
-        result = fizzbuzz(6);
-
-
-        expect(result).toBe('fizz');
-    })
-
-    it("will return buzz when 10 is supplied", function(){
-        var result;
-        result = fizzbuzz(10);
-
-
-        expect(result).toBe('buzz');
-    })
-
-    it("will return buzz when 15 is supplied", function(){
+    it("will return fizzbuzz when a multiple of 3 and 5 is supplied", function(){
         var result;
         result = fizzbuzz(15);
-
 
         expect(result).toBe('fizzbuzz');
     })
